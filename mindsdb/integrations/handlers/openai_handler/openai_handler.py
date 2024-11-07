0705673cae9f935d52df6884ab92f64503e42a73
Nik Jmaeff <nik@jmaeff.me>
2024-11-06 16:12:23 -0800

    Allow custom model_name in OpenAIHandler predict embeddings

--- mindsdb/integrations/handlers/openai_handler/openai_handler.py
+++ mindsdb/integrations/handlers/openai_handler/openai_handler.py
@@ -294,8 +294,8 @@ class OpenAIHandler(BaseMLEngine):
         # Embedding mode
         if args.get('mode', self.default_mode) == 'embedding':
             api_args = {
-                'question_column': pred_args.get('question_column', None),
-                'model': pred_args.get('model_name', 'text-embedding-ada-002'),
+                'question_column': pred_args.get('question_column') or args.get('question_column'),
+                'model': pred_args.get('model_name') or args.get('model_name'),
             }
             model_name = 'embedding'
             if args.get('question_column'):
